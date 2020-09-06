d4f645095a7026b4b76a248dbed934083669692a
inc0der <inc0der.code@gmail.com>
2020-09-06 13:22:23 -0300

    Add mcli and extend Main with default options

--- src/Main.hx
+++ src/Main.hx
@@ -1,9 +1,30 @@
-import wizard.NodePackage;
 import wizard.Builder;
+import mcli.CommandLine;
 
-class Main {
-  public static function main() {
-    Builder.newProject('./tests/temp/');
-    NodePackage.install('is-sorted');
+class Main extends CommandLine {
+  public var watch: Bool;
+
+  public function help() {
+    // print usage details
   }
+
+  public function runDefault() {
+    // run the wizard setup guide
+    init();
+  }
+
+  public function init(?path: String) {
+    // run the wizard setup guide
+    var projectPath = path == null ? path : Sys.getCwd();
+    Builder.newProject(projectPath);
+  }
+
+  public function build(path: String) {
+    // Build from source files
+  }
+
+  public static function main() {
+    new mcli.Dispatch(Sys.args()).dispatch(new Main());
+  }
+
 }
