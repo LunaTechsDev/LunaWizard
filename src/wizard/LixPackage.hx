3d19dd2c20c5c9922f728f9cc2b2d0121994ecc5
inc0der <inc0der.code@gmail.com>
2020-09-09 12:36:10 -0300

    Add LixPackage for installing haxe libs

---
+++ src/wizard/LixPackage.hx
@@ -0,0 +1,28 @@
+package wizard;
+
+class LixPackage {
+  private static function _install(args: Array<String>) {
+    args.unshift('install');
+    return NodePackage.npx('lix', args);
+  }
+
+  public static function isInstalled(): Bool {
+    return NodePackage.npx('lix', ['--help']).status;
+  }
+
+  public static function installFromGitlab(user: String, repo: String) {
+    return _install(['gitlab:${user}/${repo}']);
+  }
+
+  public static function installFromGithub(user: String, repo: String) {
+    return _install(['github:${user}/${repo}']);
+  }
+
+  public static function installFromHaxelib(name: String) {
+    return _install(['haxelib:${name}']);
+  }
+
+  public static function download(name: String) {
+    return NodePackage.npx('lix', ['download']);
+  }
+}
