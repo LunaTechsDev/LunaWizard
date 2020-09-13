c63a74f0006bfd70e0091d5cf41c54e7582591ec
inc0der <inc0der.code@gmail.com>
2020-09-13 00:06:44 -0300

    Add usage info to build command

--- src/commands/Build.hx
+++ src/commands/Build.hx
@@ -4,11 +4,21 @@ import prompts.Prompter;
 import wizard.utils.HxmlDiscovery;
 import wizard.Builder;
 
+/**
+  Build your project from the hxml file given. If no path was given
+  then LunaWizard will find all .hxml files in your project and prompt you
+  to use which ones for compilation.
+**/
 class Build extends mcli.CommandLine {
   /**
     Disable application of prettier's rules
   **/
   public var noPrettier: Bool = false;
+
+  public function help() {
+    Sys.println(this.showUsage());
+    Sys.exit(0);
+  }
 
   public function runDefault(?hxml: String) {
     // Build from source files
