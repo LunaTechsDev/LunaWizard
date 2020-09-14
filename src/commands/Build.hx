d00250348e58335f97cc2072d288da06f6886fce
inc0der <inc0der.code@gmail.com>
2020-09-14 09:50:44 -0300

    Add file watching (#12)
    * Add chokidar as dependency
    
    * Make build prompt its own function
    
    * Add file watch option to build command
    
    * Add chokidar option so we compile from source on the first watch

--- src/commands/Build.hx
+++ src/commands/Build.hx
@@ -1,5 +1,6 @@
 package commands;
 
+import chokidar.Chokidar;
 import prompts.Prompter;
 import wizard.utils.HxmlDiscovery;
 import wizard.Builder;
@@ -10,40 +11,68 @@ import wizard.Builder;
   to use which ones for compilation.
 **/
 class Build extends mcli.CommandLine {
+  private var _sourceDir: String = '';
   /**
     Disable application of prettier's rules
   **/
   public var noPrettier: Bool = false;
+
+  /** 
+    Watch for changes to source files.
+  **/
+  public var watch: Bool = false;
+
+  /**
+    The source directory to use when watching for file changes.
+  **/
+  public function sourceDir(path: String) {
+    _sourceDir = path;
+  }
+
+  private function _watch(sourceDir: String, hxml: String) {
+    Chokidar.watch(sourceDir, {
+      persistent: true,
+      usePolling: true,
+      ignoreInitial: false
+    })
+    .on('change', (path, stats) -> {
+      Builder.compileFromSource(hxml, !noPrettier);
+    });
+  }
 
   public function help() {
     Sys.println(this.showUsage());
     Sys.exit(0);
   }
 
+  private function promptBuild(hxmlPaths) {
+    var choices = hxmlPaths.map(path -> {
+      return {
+        title: path,
+        value: path,
+        description: '',
+        disabled: false,
+        selected: false
+      }
+    });
+    Prompter.call({
+      type: 'select',
+      name: 'hxmlPath',
+      message: 'Choose an hxml to build from',
+      choices: choices
+    })
+    .then((response: Dynamic) -> {
+      if (watch != null) {
+        return _watch(_sourceDir, response.hxmlPath);
+      }
+      Builder.compileFromSource(response.hxmlPath, !noPrettier);
+    });
+  }
+
   public function runDefault(?hxml: String) {
     // Build from source files
     if (hxml == null) {
-      var hxmlPaths = HxmlDiscovery.discover();
-      var choices = hxmlPaths.map(path -> {
-        return {
-          title: path,
-          value: path,
-          description: '',
-          disabled: false,
-          selected: false
-        }
-      });
-      Prompter.call({
-        type: 'multiselect',
-        name: 'hxmlPaths',
-        message: 'Choose an hxml to build from',
-        choices: choices
-      }).then((response: Dynamic) -> {
-        var paths: Array<String> = response.hxmlPaths;
-        for (path in paths) {
-          Builder.compileFromSource(path, !noPrettier);
-        }
-      });
+      promptBuild(HxmlDiscovery.discover());
       return;
     }
     Builder.compileFromSource(hxml);
